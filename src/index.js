10c20c4b17224ac592cfb6a557d5221c0c609a51
Sebastian Villegas <Sebastian.villegas2623@gmail.com>
2024-07-03 15:59:29 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,25 +1,73 @@
-// index.js
+let url = 'http://localhost:3000'
 
-// Callbacks
-const handleClick = (ramen) => {
-  // Add code
+
+const displayRamens = (ramen) => {
+  console.log(ramen)
+  //Obtain the element that we need (div)
+let ramenMenuDivTag = document.getElementById("ramen-menu")
+//create the elements that we need (img,)
+let imgRam= document.createElement("img")
+imgRam.src = ramen.image
+
+ramenMenuDivTag.append(imgRam)
+
+imgRam.addEventListener("click", (e) => handleClick(ramen))
+}
+
+
+function fetchFood() {
+  fetch(`${url}/ramens`)
+    .then ((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw "ALARM ALARM ALARM!";
+      };
+    })
+    .then ((data) =>{
+      displayRamens(data[1]);
+
+      data.forEach((curRamen)=>{displayRamens(curRamen)})
+      
+    }) 
 };
+
+fetchFood();
+
+
+const handleClick = (ramen) => {
+   //obtain the Id and Classes we need(img, H2, H3)
+   let imageClass = document.getElementsByClassName("detail-image")[0];
+   let restaurantNameH2 = document.getElementsByClassName("name")[0];
+   let ramenNameH3 = document.getElementsByClassName("restaurant")[0];
+
+   imageClass.src = ramen.image;
+   restaurantNameH2.textContent= ramen.restaurant;
+   ramenNameH3.textContent = ramen.name;
+
+   let spanRatingDisplay = document.getElementById('rating-display');
+   let pCommentDisplay = document.getElementById('comment-display');
+
+   spanRatingDisplay.textContent = ramen.rating;
+    pCommentDisplay.textContent = ramen.comment; 
+};
+const newForm = document.getElementById("new-ramen")
 
 const addSubmitListener = () => {
-  // Add code
-}
+  newForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newRamens = {
+      image: e.target.image.value,
+      restaurant: e.target.restaurant.value,
+      name: e.target.name.value,
+      rating: e.target.rating.value,
+      comment: e.target.elements['new-comment'].value
+    };
 
-const displayRamens = () => {
-  // Add code
+    displayRamens(newRamens);
+    e.target.reset();
+  })
 };
-
-const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
-}
-
-main()
-
 // Export functions for testing
 export {
   displayRamens,
@@ -27,3 +75,9 @@ export {
   handleClick,
   main,
 };
+
+const main = () => {
+  displayRamens()
+  addSubmitListener();
+};
+main()
